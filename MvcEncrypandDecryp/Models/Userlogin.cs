53402eca23d68884b94878102989d4c4c9e8d90d
Ranjan Kumar <ranjan7823@gmail.com>
2017-09-02 23:09:42 +0530

    First Commit

---
+++ MvcEncrypandDecryp/Models/Userlogin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcEncrypandDecryp.Models
+{
+    public class Userlogin
+    {
+        [Required(ErrorMessage = "Enter Username")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Enter Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string HDUser { get; set; }
+        // hidden fields for username
+        public string HDpass { get; set; }
+        // hidden fields for password
+    }
+}
